a6669053e5c8884546d9e242d3fa925ea40f5bd6
Alexey <batin.alexey@gmail.com>
2016-11-06 23:20:36 +0300

    v3.0

--- AlgoritmNuitona.c
+++ AlgoritmNuitona.c
@@ -4,24 +4,27 @@
 #include <Windows.h>
 int main(){
 	int k,ind;
-	double a,res;
+	double a,res=1;
 	SetConsoleOutputCP(1251);
 	printf("\t\tАлгоритм Ньютона\n");
-	printf("Введіть степінь кореня");
-	scanf("%d",&k);
-	printf("Введіть число а");
-	scanf("%lf",&a);
-	if (k==0) printf("Помилка при введені значень");
-	else if ((a<0 && k%2==0) || (a==0) && (k<0) ) printf("Корінь не існує");
-	else if (k==1 && a==0) printf("Корінь = %lf",res);
-	else if (k==-1) { res = 1/a; printf("Корінь = %lf",res); }
-	else {
-		do
-		{
-			res = (a/pow(1,k - 1) + (k - 1)*1)/k;
+	while(printf("Введіть степінь кореня: "),scanf("%d",&k)){
+	if (k==0) printf("Помилка при введені значень\n"); 
+	else { 
+		printf("Введіть число а: ");
+		scanf("%lf",&a);
+		if ((a<0 && k%2==0) || (a==0) && (k<0) ) printf("Корінь не існує\n");
+		else if (k==1 && a==0) {res = a; printf("Корінь степеня %d з %g = %g\n",k,a,res); }
+		else if (k==-1) { res = 1/a; printf("Корінь степеня %d з %g = %g\n",k,a,res); }
+		else {
+			do
+			{
+			res = (a/pow(res,k - 1) + (k - 1)*res)/k;
+			}
+			while(fabs(a/pow(res, k) - 1.) > 0.1e-5); 
+		printf("Корінь степеня %d з %g = %g\n",k,a,res);
 		}
-		while(fabs(a/pow(1,k) - 1.) > 0.1e-5);
-		printf("Корінь = %lf",res);
+	}
+	printf("Якщо бажаєте вийти з програми , то введіть будь-яку букву або символ.\n----------------------------------------------------\n");
 	}
 	system("pause");
 }
